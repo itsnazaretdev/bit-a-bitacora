aba99c42efefc6f54269c0ffc55531fade5de3b8
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-08-29 19:13:26 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://tu-usuario.github.io',
+  // Cambia esto cuando despliegues (Netlify, Vercel, GitHub Pages...)
+});
